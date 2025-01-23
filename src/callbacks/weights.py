210092d450d301a2f0c79b8730afa9d6a65400ee
Guilherme Pereira <gdrp77mdl@gmail.com>
2025-01-23 15:43:38 +0000

    warning fixed

--- src/callbacks/weights.py
+++ src/callbacks/weights.py
@@ -1,10 +1,15 @@
 import pytorch_lightning as pl
-
+import numpy as np
 
 class WeightsPrinterCallback(pl.Callback):
+    stats = {}
 
-    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
-        self._print_weights(pl_module)
+    # def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
+    #     self._print_weights(pl_module)
+
+    def on_train_end(self, trainer, pl_module):
+        # self._print_weights(pl_module)
+        self._evaluate_weights(pl_module)
 
     def _print_weights(self, pl_module):
         state_dict = pl_module.state_dict()
@@ -14,4 +19,24 @@ class WeightsPrinterCallback(pl.Callback):
                 print(f"Shape: {tensor.shape}")
                 print(f"Values:\n{tensor}")
 
-        print('\n---------\n')
+    def _evaluate_weights(self, pl_module):
+        state_dict = pl_module.state_dict()
+        for name, tensor in state_dict.items():
+            if 'weight' in name:
+                tensor_np = tensor.cpu().numpy()
+                self.stats[name] = {
+                    'shape': tensor_np.shape,
+                    'input_size': tensor_np.shape[1],
+                    'output_size': tensor_np.shape[0],
+                    'mean': np.mean(tensor_np).item(),
+                    'median': np.median(tensor_np).item(),
+                    'std': np.std(tensor_np).item(),
+                    'max': np.max(tensor_np).item(),
+                    'min': np.min(tensor_np).item()
+                }
+
+    def print_stats(self):
+        for name, stats in self.stats.items():
+            print(f"\nLayer: {name}")
+            for stat_name, stat_value in stats.items():
+                print(f"{stat_name}: {stat_value}")
